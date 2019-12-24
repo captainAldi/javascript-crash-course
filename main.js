a320fd8073c7184baf0911035c47ff2360899ecd
captainAldi <renaldiyulvianda@yahoo.come>
2019-12-24 14:43:46 +0700

    perkenalan tipe data

---
+++ main.js
@@ -0,0 +1,15 @@
+//string, numbers, boolean, null, undefined
+
+const nama			= 'Budi'
+const umur			= '30'
+const rating	= 4.7
+const isCool	= true
+const a 					= null
+const b 					= undefined
+
+// inisialisasi let tanpa pemberian nilai juga dapat menyembabkan undefined
+let c;
+
+//typeOf + nama variable, bisa menujukan tipe data yang digunakan
+//contoh: typeof nama
+console.log(typeof nama)
