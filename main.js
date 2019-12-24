251a8048a4203b323190dfcaa2d545ad2d34ffb1
captainAldi <renaldiyulvianda@yahoo.come>
2019-12-24 15:02:00 +0700

    Properties dan Methods pada string serta readme file

--- main.js
+++ main.js
@@ -1,12 +1,29 @@
 //string, numbers, boolean, null, undefined
 
-const nama			= 'Budi'
-const umur			= '30'
+const nama							= 'Budiyanto'
+const namaBanyak = 'Naruto, Sasuke, Sakura'
+const umur							= '30'
 
-//Concatenation
+//String Properties and Methods
 
-//Cara 02 - ES2015 ++
+//properties tidak memliki ()
+console.log(nama.length)
 
-//Template String atau Template literals
-//Bukan ' / " tapi `
-console.log(`Nama Saya adalah ${nama} dan Umur Saya adalah ${umur}`)
+//Method memiliki ()
+//Method adalah function yang berkaitan dengan object
+console.log(nama.toLowerCase())
+console.log(nama.toUpperCase())
+console.log(nama.substring(0, 5))
+
+//Gabungkan dengan method lain
+console.log(nama.substring(0, 5).toUpperCase())
+
+//Pisahkan String menjadi array
+//GUnakan method split()
+//Parameter yang digunakan ada beberapa
+
+//Contoh: jika per huruf maka biar kan saja jadi split('')
+console.log(nama.split(''))
+
+//Cntoh: jika per tanda comma maka jadi split(', ')
+console.log(namaBanyak.split(', '))
