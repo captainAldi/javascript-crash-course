6a07a48fda03301ea9d3f1cbc385a67344ef71eb
captainAldi <renaldiyulvianda@yahoo.come>
2019-12-24 14:20:35 +0700

    coba 01 - let - reassign

---
+++ main.js
@@ -0,0 +1,14 @@
+//let, const
+
+//Let bisa dilakukan reassign
+//atau pemberian nilai secara berulang
+
+//contoh
+
+let umur = 21;
+umur = 22;
+
+console.log(umur)
+
+//Bisa dilihat console pada browser menampilkan pemberian nilai terkahir
+//Ini artinya variable let bsa di reassign
