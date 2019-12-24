089daf54a53f86304711e29bb63900990d4c160f
captainAldi <renaldiyulvianda@yahoo.come>
2019-12-24 14:47:39 +0700

    Concatenation - Cara 01

--- main.js
+++ main.js
@@ -2,14 +2,8 @@
 
 const nama			= 'Budi'
 const umur			= '30'
-const rating	= 4.7
-const isCool	= true
-const a 					= null
-const b 					= undefined
 
-// inisialisasi let tanpa pemberian nilai juga dapat menyembabkan undefined
-let c;
+//Concatenation
 
-//typeOf + nama variable, bisa menujukan tipe data yang digunakan
-//contoh: typeof nama
-console.log(typeof nama)
+//Cara 01
+console.log('Nama Saya Adalah '+ nama + ' dan Umur Saya Adalah '+ umur)
